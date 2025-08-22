4802e2847aa1eee70afaf642984f504488b5ab46
felipe.ghidini <felipe.ghidini@contabilizei.com.br>
2025-08-22 16:40:13 -0300

    Commit inicial com configuracao do banco h2 e classe personagem

---
+++ src/main/java/entities/Personagem.java
@@ -0,0 +1,77 @@
+package entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_personagem")
+public class Personagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private String imagem;
+    private String vila;
+    private String habilidade;
+
+    public Personagem() {
+    }
+
+    public Personagem(Long id, String nome, String descricao, String imagem, String vila, String habilidade) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.imagem = imagem;
+        this.vila = vila;
+        this.habilidade = habilidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getVila() {
+        return vila;
+    }
+
+    public void setVila(String vila) {
+        this.vila = vila;
+    }
+
+    public String getHabilidade() {
+        return habilidade;
+    }
+
+    public void setHabilidade(String habilidade) {
+        this.habilidade = habilidade;
+    }
+}
